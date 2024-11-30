d73859c5316de682aa73f7763d14137ce29bd441
Alexander <fix1in1it@gmail.com>
2024-11-30 21:32:42 +0300

    add mock

--- backend/bmstu-front-app/src/modules/mocks.ts
+++ backend/bmstu-front-app/src/modules/mocks.ts
@@ -22,6 +22,16 @@ export const SECTIONS_MOCK: SectionsResult = {
             instructor: "Петров Петр Петрович",
             duration: 90,
             imageUrl: "http://127.0.0.1:9000/bmstu-sport/1.png"
+        },
+        {
+            pk: 1,
+            title: "mock-Хоккей",
+            description: "У этой секции нет описания",
+            location: "СК МГТУ",
+            date: "2024-10-22",
+            instructor: "Петров Петр Петрович",
+            duration: 90,
+            imageUrl: "http://127.0.0.1:9000/bmstu-sport/3.png"
         }
     ],
     number_of_sections: 0,
